6bd778ac5f8b1bcf0e4de5fd31be1916f96e9a7c
EUGEN OVIDIU RUSU <rusu.eugen174035@student.ubbcluj.ro>
2021-12-04 11:33:20 +0200

    Add project files.

---
+++ Data/Rusu_Eugen_Ovidiu_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Rusu_Eugen_Ovidiu_Lab8.Models;
+
+namespace Rusu_Eugen_Ovidiu_Lab8.Data
+{
+    public class Rusu_Eugen_Ovidiu_Lab8Context : DbContext
+    {
+        public Rusu_Eugen_Ovidiu_Lab8Context (DbContextOptions<Rusu_Eugen_Ovidiu_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Rusu_Eugen_Ovidiu_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Rusu_Eugen_Ovidiu_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Rusu_Eugen_Ovidiu_Lab8.Models.Category> Category { get; set; }
+    }
+}
